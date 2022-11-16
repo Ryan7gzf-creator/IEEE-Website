49572742388afe02e5580c2c67cfae2442fcac12
Ryayoon <ryoo514@aucklanduni.ac.nz>
2022-11-17 01:59:39 +1300

    added most components for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,52 @@
 import './Home.css';
+import Join from '../../assets/First-image.svg';
+import Find from '../../assets/Second-image.svg';
 
 const Home = () => {
     return (
-        <h1>Hello, im home page!</h1>
+        <>
+            <div className="row">
+                <div className="Joining">
+                    <h1>
+                        Jumpstart
+                        <br/>
+                        Your Future
+                    </h1>
+                    <p>
+                        IEEE is a professional home to help students
+                        <br/>
+                        transition to industry by providing valuable skills
+                        <br/>
+                        and a like minded community of individuals.
+                    </p>
+                    <button className="link-buttons"><h4>JOIN US</h4></button>
+                </div>
+                <div className="First-Picture">
+                    <img className="Join-Image" src={Join} alt="Join-Us"/>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="Second-Picture">
+                    <img src={Find} alt="Join-Us"/>
+                </div>
+                <div className="Find-More">
+                    <h2>
+                        Helping Students
+                        <br/>
+                        Transition To Industry
+                    </h2>
+                    <p>
+                        Home to over 500,000 members globally, IEEE UoA
+                        <br/>
+                        Student Branch is here to foster a future of engineering
+                        <br/>
+                        with you.
+                    </p>
+                    <button className="link-buttons"><h4>FIND OUT MORE</h4></button>
+                </div>
+            </div>
+        </>
     );
 }
 
